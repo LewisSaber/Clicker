e9ea9de9bc0562b26d08d36b3fc03d6b4ff5875a
lewissaber <mama040508@gmail.com>
2022-01-30 01:13:08 +0200

    lower crafts notation min number

--- craftamont.js
+++ craftamont.js
@@ -14,11 +14,11 @@ amounts[7] = 32
 function updateCraftButtons() {
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].innerHTML =
-      "<img src='Craft.jpeg'>Craft " + (amounts[i] * tocraft).formateNumber()
+      "<img src='Craft.jpeg'>Craft " + (amounts[i] * tocraft).formateNumber(1e4)
   }
   for (let i = 0; i < materials.length; i++) {
     craftmaterials[i + 1] = mats[i] * tocraft
-    materials[i].innerText = craftmaterials[i + 1].formateNumber()
+    materials[i].innerText = craftmaterials[i + 1].formateNumber(1e4)
   }
 }
 updateCraftButtons()
